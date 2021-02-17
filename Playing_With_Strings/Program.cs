75660a20b5ada467505115f346a092d38adc639c
Nicole Hamilton <Nicole@NicolesLaptop.local>
2021-02-17 13:14:53 -0800

    set up commit

---
+++ Playing_With_Strings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Playing_With_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
