0a54ad70bb9b4b52c092a3547c65420badb4008e
Nicole Hamilton <Nicole@NicolesLaptop.local>
2021-02-17 13:33:02 -0800

    playing with a few string methods including Substring, ToLower, ToUpper, Trim, IndexOf, IsNullOrWhiteSpace, and String.Format

--- Playing_With_Strings/Program.cs
+++ Playing_With_Strings/Program.cs
@@ -11,7 +11,30 @@ namespace Playing_With_Strings
             int num1 = Int32.Parse(stringOne);
             int num2 = Int32.Parse(stringTwo);
             int result = num1 + num2;
-            Console.Write(result);
+            Console.WriteLine(result);
+
+            //SubString(int32) is used to get the substring from the string, starting at the specified index
+            //ToLower() used to convert the string to lowercase
+            //ToUpper() used to convert string to uppercase
+            //Trim() used to trim all leading and trailing white space from the string
+            //IndexOf(string) used to get the index of the string or char inside the string
+            //InNullorWhiteSpace returns true if the string is null or blank, else returns false
+
+            string firstName = "Nikki";
+            string lastName = "Hamilton";
+            string fullName = string.Concat(" ", firstName, lastName, " ");
+            Console.WriteLine(firstName.Substring(2)); //output: kki
+            Console.WriteLine(firstName.ToLower()); //output: nikki
+            Console.WriteLine(firstName.ToUpper()); //output: NIKKI
+            Console.WriteLine(fullName.Trim()); //output: NikkiHamilton
+            Console.WriteLine(firstName.IndexOf('i')); //output: 1 (only grabs first instance of 'i')
+            Console.WriteLine(String.IsNullOrWhiteSpace(firstName)); //output: false
+
+            //String.Format methos is used to insert the object or variable value inside any string
+            //with String.Format we replace the value in the specified format
+            //Syntax: String.Format("any string {index}", object);
+
+            Console.WriteLine(String.Format("My name is {0}", firstName));
         }
     }
 }
